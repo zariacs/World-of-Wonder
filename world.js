c7f56462b05b968fe13e05acc5bdea91b5eabcf2
Zaria Chen Shui <zariachenshui@gmail.com>
2023-01-06 15:03:22 -0500

    Added project files

---
+++ world.js
@@ -0,0 +1,33 @@
+let buttons = document.querySelectorAll('button');
+let result = document.getElementById('result');
+
+buttons.forEach(btn =>
+    btn.onclick = (event) => {
+        event.preventDefault();
+        let search = document.getElementById("country").value;
+        console.log(btn.id);
+        if (btn.id == 'lookup-country') {
+            var params = {
+                country: search,
+                lookup: 'country'
+            };
+        } else if (btn.id == 'lookup-cities') {
+            var params = {
+                country: search,
+                lookup: 'cities'
+            };
+        }
+        console.log(params);
+        let url = 'http://localhost/Projects/World-Database-Lookup/world.php?' + new URLSearchParams(params);
+
+        fetch(url)
+            .then(response => response.text())
+            .then(data => {
+                // Add country data to result div
+                result.innerHTML = data;
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+)
